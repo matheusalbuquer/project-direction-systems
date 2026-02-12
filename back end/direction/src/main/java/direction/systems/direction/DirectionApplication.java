40d32212efd074ad6f71052066f591c4213b8171
matheus albuquer <mj898926@gmail.com>
2026-02-12 14:00:21 -0300

    iniciando o projeto

---
+++ back end/direction/src/main/java/direction/systems/direction/DirectionApplication.java
@@ -0,0 +1,13 @@
+package direction.systems.direction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DirectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DirectionApplication.class, args);
+	}
+
+}
---
+++ back end/direction/src/test/java/direction/systems/direction/DirectionApplicationTests.java
@@ -0,0 +1,13 @@
+package direction.systems.direction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DirectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
